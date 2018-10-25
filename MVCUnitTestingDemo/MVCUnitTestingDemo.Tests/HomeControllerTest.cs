050a5cd1558e28eecab87da65a534fe486abc33d
Ashish <Ashish@DESKTOP-D1NHQPL>
2018-10-25 06:23:19 +0530

    change in controller

--- MVCUnitTestingDemo/MVCUnitTestingDemo.Tests/HomeControllerTest.cs
+++ MVCUnitTestingDemo/MVCUnitTestingDemo.Tests/HomeControllerTest.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MVCUnitTestingDemo;
-using MVCUnitTestingDemo.Controllers;
+using MVCUnitTestingDemo.MyControllers;
 using System.Web.Mvc;
 
 namespace MVCUnitTestingDemo.Tests
